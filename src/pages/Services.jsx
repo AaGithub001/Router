7ee05cbf638dbd92e65eee0158cde2cf525cd041
AaGithub001 <aasigning001@gmail.com>
2023-10-05 13:28:03 +0545

    conditioning

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Services() {
+    const [check, setCheck]= useState(true);
+    const toggleValue = () => {
+        setCheck(prev => !prev)
+    }
   return (
-    <div>Services</div>
+    <>
+    <div>{check ? "true":"false"}</div>
+    <button onClick={toggleValue}> </button>
+    Toggle
+    </>
   )
 }
 
